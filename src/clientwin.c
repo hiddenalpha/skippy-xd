f42faec319e9e0606f29dcfefba62859d466ca0e
Andreas Fankhauser hiddenalpha.ch <23085769+hiddenalpha@users.noreply.github.com>
2025-11-23 13:11:24 +0100

    Revert the one that likely is unreachable.

--- src/clientwin.c
+++ src/clientwin.c
@@ -759,7 +759,7 @@ clientwin_tooltip(ClientWin *cw) {
 
 		if (cw->mode == CLIDISP_DESKTOP) {
 			label = wm_get_desktop_name(mw->ps, cw->slots);
-			len = (!label) ? 0 : strlen((char*)label);
+			len = strlen((char*)label);
 		}
 		else if (ps->o.tooltip_option == 0) {
 			label = wm_get_window_title(ps, cw->wid_client, &len);
